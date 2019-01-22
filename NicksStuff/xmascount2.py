ed61277937e8a96490aa2449f393ccf7254ea964
therealcarnivor <46380907+therealcarnivor@users.noreply.github.com>
2019-01-22 08:51:49 +0000

    another version of counter

---
+++ NicksStuff/xmascount2.py
@@ -0,0 +1,12 @@
+import datetime
+
+dt = datetime.datetime
+
+currentYear = dt.now().year
+delta = dt(currentYear, 12, 25) - dt.now()
+
+if dt.now() >= dt(currentYear, 12, 25):
+print ('*** Christmas Week ***')
+else:
+print ('Today's date is', dt.now())
+print ('There are ',delta.days, ' to Christmas')
