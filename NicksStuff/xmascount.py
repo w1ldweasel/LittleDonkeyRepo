957ba9f0cd2425dcc8450c832b44a56408486e00
therealcarnivor <46380907+therealcarnivor@users.noreply.github.com>
2019-01-22 08:51:07 +0000

    xmas counter version 1

---
+++ NicksStuff/xmascount.py
@@ -0,0 +1,8 @@
+import datetime
+
+dt = datetime.datetime
+currentYear = dt.now().year
+delta = dt(currentYear, 12, 25) - dt.now()
+
+print ('Today's date is', dt.now())
+print ('There are ',delta.days, ' to Christmas')
