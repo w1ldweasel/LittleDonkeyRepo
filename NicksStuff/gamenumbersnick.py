e09d4b87085a5c379171915bff417a188432b889
therealcarnivor <46380907+therealcarnivor@users.noreply.github.com>
2019-01-22 09:12:21 +0000

    Carns Modded Version

---
+++ NicksStuff/gamenumbersnick.py
@@ -0,0 +1,52 @@
+# ==== Code club
+# ==== Exercise 3 Nicks Version
+#
+import random
+
+# Intro message
+def askgamechoice(*answer):
+  print('\n\n')        
+  print ("Which game would you like to play? Enter \'L\' for Lotto, \'E\' for Euromillions.")
+  game_choice = input('Choice >')
+  if game_choice.lower() == 'l':
+    generate_lotto_numbers()
+  elif game_choice.lower() == 'e':
+    generate_euro_numbers()
+
+# function that returns 6 random numbers
+def generate_lotto_numbers():
+    lotto_numbers = []
+    for i in range(6):
+        lotto_numbers = new_random_list(lotto_numbers)
+    print('These are your six numbers:')
+    print(lotto_numbers)
+
+# function that returns 5 random main numbers and 2 'star' numbers
+def generate_euro_numbers():
+    euro_numbers = []
+    star_numbers = []
+    for i in range(5):
+        euro_number = random.randint(1,51)  
+        euro_numbers.append(euro_number)
+    for i in range(2):
+        star_number = random.randint(1,13)  
+        star_numbers.append(star_number)
+
+    print('These are your five main numbers and two star numbers:')                
+    print(euro_numbers)
+    print(star_numbers)
+
+def new_random_list(number_list_arg):
+    temp_list = number_list_arg
+    current_list_size = len(temp_list)
+    flag = len(temp_list)
+    while flag == current_list_size:
+        new_number = random.randint(1,60)
+        if new_number not in temp_list:
+            temp_list.append(new_number)
+            flag = flag+1
+            return temp_list
+        
+#main()
+if __name__ == "__main__":
+  askgamechoice()
