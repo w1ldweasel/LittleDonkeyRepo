0a0f05621d34f49834e28a56f654287f0471da0c
maxxmcf <maxinemcfarlane@yahoo.co.uk>
2019-01-24 19:42:31 +0000

    Maxine Task 1
    Hello World program

---
+++ MaxineFolder/HWmax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 23 19:57:24 2019
+
+@author: maxinemcfarlane
+"""
+
+name = (input("Please tell me your name (please put your name in quotes!): "))
+    
+print ('hello world')
+print ('hello ' + name)
+
+
+
