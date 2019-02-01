6487cd55b7871dcaa97729570cc9e51fb860a53d
dafydd <dafydd@example.com>
2019-02-01 14:24:55 +0000

    updated helloworld.py with commandline parameter for name

--- daf/helloworld.py
+++ daf/helloworld.py
@@ -1,5 +1,12 @@
+# /usr/bin/python3
+
 def sayhi(myname):
     print("Hello World")
     print("Hello " + myname)
 
-sayhi('Dafydd')
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("include your name as an arguement you prick")
+    else:
+        sayhi(sys.argv[1])
