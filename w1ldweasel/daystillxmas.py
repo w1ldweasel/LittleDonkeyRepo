b2bd47c856795a28546813b7f0bd9fcca07657bd
w1ldweasel <maxhyng@gmail.com>
2019-02-01 22:46:54 +0000

    copying file over for exercises on wiki

---
+++ w1ldweasel/daystillxmas.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+# script to output the number of days until xmas
+
+import datetime
+
+dt = datetime.datetime
+delta = dt(2019, 12, 25) - dt.now()
+
+print ('Today\'s date is', dt.now())
+print ('There are ',delta.days, ' to Christmas')
+
+#print(delta.days)
+
+
