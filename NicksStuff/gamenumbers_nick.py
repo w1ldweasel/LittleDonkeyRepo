b8a11f3fb7466249932ffe8941cc71f9738ef063
therealcarnivor <46380907+therealcarnivor@users.noreply.github.com>
2019-02-04 07:14:31 +0000

    Update gamenumbers_nick.py

--- NicksStuff/gamenumbers_nick.py
+++ NicksStuff/gamenumbers_nick.py
@@ -1,6 +1,7 @@
 # ==== Code club
 # ==== Exercise 3 Nicks Version
-#
+# ==== Added Sorting to Lotto and Euro and Star Numbers
+
 import random
 
 # Intro message
@@ -18,7 +19,9 @@ def askgamechoice(*answer):
 def generate_lotto_numbers():
     lotto_numbers = []
     for i in range(6):
-        lotto_numbers = new_random_list(lotto_numbers)
+        lotto_number = random.randint(1,59)
+        lotto_numbers.append(lotto_number)
+        lotto_numbers.sort()
     print('These are your six numbers:')
     print(lotto_numbers)
 
@@ -29,9 +32,11 @@ def generate_euro_numbers():
     for i in range(5):
         euro_number = random.randint(1,51)  
         euro_numbers.append(euro_number)
+        euro_numbers.sort()
     for i in range(2):
         star_number = random.randint(1,13)  
         star_numbers.append(star_number)
+        star_numbers.sort()
 
     print('These are your five main numbers and two star numbers:')                
     print(euro_numbers)
@@ -51,4 +56,3 @@ def new_random_list(number_list_arg):
 #main()
 if __name__ == "__main__":
   askgamechoice()
-
