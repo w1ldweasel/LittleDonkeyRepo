7b159c35a7a557ce62034fc37b5363059f7c2b27
maxxmcf <maxinemcfarlane@yahoo.co.uk>
2019-02-06 21:43:22 +0000

    Update HWmax.py
    updated HWmax with DoB Q and age logic

--- MaxineFolder/HWmax.py
+++ MaxineFolder/HWmax.py
@@ -5,11 +5,43 @@ Created on Wed Jan 23 19:57:24 2019
 
 @author: maxinemcfarlane
 """
+from datetime import date
 
-name = (input("Please tell me your name: "))
+
+def proceed_hwnumber():
     
-print ('hello world')
-print ('hello ' + name)
+#user to supply input to program
+    name = (input("Please tell me your name: "))
+    yearob = int(input("Please tell me the year you were born (YYYY): "))
+    monthob = int(input("Please tell me what number month you were born: "))
+    dateob = int(input("Please tell me the date you were born: "))
+
+
+#calculate users age today
+    today = date.today()
+    yearage = today.year - yearob
+    monthage = today.month - monthob
+    dayage = today.day - dateob
+
+
+#output to user    
+    print ('Hello world')
+    print ('Hello ' + name)
+    if monthage < 0 or monthage == 0 and dayage < 0:
+        print ("You are ", yearage - 1," years old today")
+    else:
+        print ("You are ", yearage ," years old today")
+
+
+#calculate if users birthday and print HB if so
+    if dateob == today.day and monthob == today.month:
+        print("Happy Birthday " + name)
+
+
+#code to run first function if program is started in interpreter
+if __name__ == "__main__":
+  proceed_hwnumber()
+  
 
 
 #Hi Max, if you test the code it works as Max because Python automatically creates a string from the user input
