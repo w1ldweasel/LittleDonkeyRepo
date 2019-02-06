646572b2ae1bc4daf0660cedc4c33dd1691c4648
an13b_786@hotmail.co.uk <1326Mercedes#1>
2019-02-06 21:25:46 +0000

    Create shoppingdays_till_xmas.py
    Uploading Task 5

---
+++ AniebsFolder/Feb Assignments/shoppingdays_till_xmas.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  5 22:27:20 2019
+
+@author: Anieb
+"""
+
+import datetime
+
+now = datetime.datetime.now()
+dt = datetime.datetime
+
+currentyear = now.year
+
+delta = dt(currentyear, 12, 2) - now
+print ("current Date: " + now.strftime("%d - %b - %Y"))
+
+SavingTarget = input ("Please enter how much you would like to save £")
+SavingTargetConvert = int(SavingTarget) * 1
+print ("in order to save £" + SavingTarget + ' for Cyber monday') 
+
+MonthCalculation = (delta.days / 7 / 4)
+
+
+MonthlySaving = (SavingTargetConvert / MonthCalculation)
+MonthlySavingAmount = float(MonthlySaving) *1
+MonthlySavingAmount = str(round(MonthlySavingAmount, 2))
+
+
+MonthCalculation = str(round(MonthCalculation, 2))
+SavingTargetConvert = str(round(SavingTargetConvert, 2))
+
+print ("You need to save £" + MonthlySavingAmount + " a month for the next " + MonthCalculation + " Months")
+print ("There are only",delta.days, 'days to Cyber Monday Get Saving to Reach Your Goal!!')
+
+
