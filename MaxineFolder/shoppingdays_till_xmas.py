eac722e3fe7450a55a36e8a8fc7a325eb8337383
maxxmcf <maxinemcfarlane@yahoo.co.uk>
2019-02-11 15:15:40 +0000

    draft shopping days

--- MaxineFolder/shoppingdays_till_xmas.py
+++ MaxineFolder/shoppingdays_till_xmas.py
@@ -3,32 +3,53 @@
 # script to output the number of days until xmas
 
 import datetime
+from datetime import date
 
-dt = datetime.datetime
+def proceed_shopdays():
+    
+    dt = datetime.datetime
 
-thisyear = dt.now().year
-delta = dt(thisyear, 12, 25) - dt.now()
+    thisyear = dt.now().year
+    delta = dt(thisyear, 12, 25) - dt.now()
 
-#date formating
+#date formatting
+    print ("Today\'s date is " , str(date.today().strftime("%d %b %Y")))
+
 
 #example of how much monthly savings give for xmas
-print ("If you would like to save £1000 for Christmas, you will need to save " 
-       thisamount " pounds a month from now")
+    exsave = 1000
+#    cmdelta = dt(thisyear, ) - dt.now()
+    
+#    weekday() == 0
+#    26 nov to dec 2
+    
+    months = 12 - dt.now().month
+    thisamount = exsave / months
+    
+    print ("If you would like to save £1000 for Christmas, you will need to save £", round(thisamount), " a month from now")
 
+   
 #user input
-usersave = input("How much do you need to save for Christmas in Pound Sterling?")
+    usersave = int(input("How much do you need to save for Christmas in Pound Sterling?"))
 
+    
 #savings calculation
+    otheramount = usersave / months
 
 
 #output to user
-print ("To have " usersave " saved for Christmas, you will need to save '
-       otheramount ' per month")
-print("This will give you " usersave " to spend on Cyber Monday deals!")
+    print("To have £", usersave, " saved for Christmas, you will need to save £", round(otheramount), " per month")
+    print("This will give you £", usersave, " to spend on Cyber Monday deals!")
+#
+#
+#    print ('There are ',cmdelta.days, ' days to Cyber Monday')
+    print ('There are ',delta.days, ' days to Christmas')
 
 
-print ('There are ',cmdelta.days, ' days to Cyber Monday')
-print ('There are ',delta.days, ' days to Christmas')
+#code to run first function if program is started in interpreter
+if __name__ == "__main__":
+  proceed_shopdays()
+
 
 
 
