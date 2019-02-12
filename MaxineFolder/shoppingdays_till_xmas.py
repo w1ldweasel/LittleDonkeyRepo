de76b5e621214bd55a231749ddba163062939531
maxxmcf <maxinemcfarlane@yahoo.co.uk>
2019-02-12 11:36:23 +0000

    shopping days draft

--- MaxineFolder/shoppingdays_till_xmas.py
+++ MaxineFolder/shoppingdays_till_xmas.py
@@ -1,9 +1,11 @@
 #! /usr/bin/python
 
-# script to output the number of days until xmas
+# Written by Maxine
+# Date 8/2/2108
 
 import datetime
 from datetime import date
+
 
 def proceed_shopdays():
     
@@ -18,14 +20,18 @@ def proceed_shopdays():
 
 #example of how much monthly savings give for xmas
     exsave = 1000
-#    cmdelta = dt(thisyear, ) - dt.now()
+  
+#    weekday() == 0 (MON)
+#    weekday() == 4 (THUR)
+#   last sunday in Nov = thanksgiving
+#   cyber monday is monday following that
     
-#    weekday() == 0
-#    26 nov to dec 2
+#    cmdelta = dt(cyber mon?) - dt.now()
     
     months = 12 - dt.now().month
     thisamount = exsave / months
     
+    print (" ")
     print ("If you would like to save £1000 for Christmas, you will need to save £", round(thisamount), " a month from now")
 
    
@@ -38,10 +44,11 @@ def proceed_shopdays():
 
 
 #output to user
+    print (" ")
     print("To have £", usersave, " saved for Christmas, you will need to save £", round(otheramount), " per month")
     print("This will give you £", usersave, " to spend on Cyber Monday deals!")
-#
-#
+
+    print (" ")
 #    print ('There are ',cmdelta.days, ' days to Cyber Monday')
     print ('There are ',delta.days, ' days to Christmas')
 
