4aa45ad67e945ccc84188ea0780e9e8330108e82
maxxmcf <maxinemcfarlane@yahoo.co.uk>
2019-02-15 16:58:39 +0000

    Task 3 completed
    removed comments on task 3

--- MaxineFolder/mm_game_numbers.py
+++ MaxineFolder/mm_game_numbers.py
@@ -61,7 +61,6 @@ def generate_lotto_numbers():
     
     if print_choice == 'Y' or print_choice =='y':
         f = open("lotto_numbers.txt", "a+")
-    # sort writing date to file - THIS DOESNT WORK YET!
         f.write("Lotto Numbers - %s, played on %s\n" %(lotto_numbers, date.today()))
         f.close()
         print('Your numbers have been saved')
@@ -94,7 +93,6 @@ def generate_euro_numbers():
     
     if print_choice == 'Y' or print_choice =='y':
         f = open("euro_numbers.txt", "a+")
-# sort writing date to file - THIS DOESNT WORK YET!
         f.write("Euro Numbers - %s, Star Numbers - %s, played on %s\n" %(euro_numbers, star_numbers, date.today()))
         f.close()
         print('Your numbers have been saved')
