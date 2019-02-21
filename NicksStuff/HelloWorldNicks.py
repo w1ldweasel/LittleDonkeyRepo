5f089e986002919b9be79d7f3f5003bc84f9af68
therealcarnivor <46380907+therealcarnivor@users.noreply.github.com>
2019-02-21 07:15:41 +0000

    Update HelloWorldNicks.py

--- NicksStuff/HelloWorldNicks.py
+++ NicksStuff/HelloWorldNicks.py
@@ -1,6 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Carneys Hello World
+"""
+
 # /usr/bin/python3
 
-print ("Hello World!")
-inputname = input ("What is your name? >")
-print ("Hello " + inputname)
-print ("I have to exit now, bye bye " + inputname)
+import datetime
+
+inputname = ""
+dobchk = ""
+
+print("Hello World!")
+
+inputname = input("What is your name? >")
+print("Hello " + inputname)
+dobask()
+
+def dobask():
+    """
+    Ask for Date of Birth
+    """
+    inputdob = input("What is your Date of Birth? (YYYY-MM-DD) > ")
+    print("You have entered > " + inputdob)
+
+    date_format = '%Y-%m-%d'
+
+    try:
+        dobchk = datetime.datetime.strptime(inputdob, date_format)
+        print(dobchk)
+        return
+    except ValueError:
+        print("Incorrect data format, should be YYYY-MM-DD")
+        dobask()
+
+print("Now for some pointless stats about your Birthday")
+
+#print("Now    :" + datetime.datetime.now())
+#delta = dobchk - dt.now()
+#print ("Its been " + delta.days, " since you were born")
+
+#Say Bye Bye
+def byebye():
+    """
+    Saye Bye Bye
+    """
+    print("I have to exit now, bye bye " + inputname)
+
+
+
+#dt = datetime.datetime
+#currentYear = dt.now().year
+#delta = dt(currentYear, 12, 25) - dt.now()
+
+#print ('Today\'s date is', dt.now())
+#print ('There are ',delta.days, ' to Christmas')
+    
