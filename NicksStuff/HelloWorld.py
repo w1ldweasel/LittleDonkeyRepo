01cfcb10b45af9bbffd909a97ffd13d5a2a24f49
therealcarnivor <46380907+therealcarnivor@users.noreply.github.com>
2019-02-21 09:45:54 +0000

    Update HelloWorld.py
    updates

--- NicksStuff/HelloWorld.py
+++ NicksStuff/HelloWorld.py
@@ -1,51 +1,63 @@
 # -*- coding: utf-8 -*-
+
+# /usr/bin/python3
 """
 Carneys Hello World
 """
 
-# /usr/bin/python3
-
 import datetime
+import time as t
 
-inputname = ""
-dobchk = ""
-
-print("Hello World!")
-
-inputname = input("What is your name? >")
-print("Hello " + inputname)
-dobask()
+INPUT_NAME = ""
+INPUT_DOB = ""
+DOB_CHK = ""
+DT = datetime.datetime
 
 def dobask():
     """
     Ask for Date of Birth
     """
-    inputdob = input("What is your Date of Birth? (YYYY-MM-DD) > ")
-    print("You have entered > " + inputdob)
+    INPUT_DOB = input("What is your Date of Birth? (YYYY-MM-DD) > ")
+    print("You have entered > " + INPUT_DOB)
 
     date_format = '%Y-%m-%d'
 
     try:
-        dobchk = datetime.datetime.strptime(inputdob, date_format)
-        print(dobchk)
+        DOB_CHK = datetime.datetime.strptime(INPUT_DOB, date_format)
+        print("Logging to system as > ", DOB_CHK)
+        t.sleep(1)
         return
     except ValueError:
         print("Incorrect data format, should be YYYY-MM-DD")
         dobask()
-
-print("Now for some pointless stats about your Birthday")
-
-#print("Now    :" + datetime.datetime.now())
-#delta = dobchk - dt.now()
-#print ("Its been " + delta.days, " since you were born")
 
 #Say Bye Bye
 def byebye():
     """
     Saye Bye Bye
     """
-    print("I have to exit now, bye bye " + inputname)
+    print("I have to exit now, bye bye " + INPUT_NAME)
 
+
+
+
+print("Hello World!")
+
+INPUT_NAME = input("What is your name? >")
+print("Hello " + INPUT_NAME)
+dobask()
+
+t.sleep(1)
+print()
+print("Now for some pointless stats about your Birthday")
+print()
+print("Now    :", DT.today())
+
+
+# fukkd from here onwards
+
+DELTA = DOB_CHK() - DT.today()
+print("Its been " + DELTA.days, " days since you were born")
 
 
 #dt = datetime.datetime
@@ -54,4 +66,3 @@ def byebye():
 
 #print ('Today\'s date is', dt.now())
 #print ('There are ',delta.days, ' to Christmas')
-    
