d0ae5e7750fa59070f85ed79aa2cc9926c2d4104
w1ldweasel <maxhyng@gmail.com>
2019-02-25 00:27:20 +0000

    Fernet encryption test

--- MaxiMoose/cryptography_services.py
+++ MaxiMoose/cryptography_services.py
@@ -22,11 +22,16 @@ def encrypt_text():
     f = open("keyfile.txt", "rb")
     key = f.read()
     print (key)
-    message = 'plaintext'
+    message = 'plaintext blah'
     encoded_message = message.encode()
     cipher = Fernet(key)
     cipher_text = cipher.encrypt(encoded_message)
     print (cipher_text)
+    
+    #Test the decryption
+    plaintext_message = cipher.decrypt(cipher_text)
+    print (plaintext_message)
+    
 
 def base64_encode():
     encoded_data = base64.b64encode("Base64 encode this text")
