1ea17e1e56d159561354ab6cdeb8962c62e9c5c9
w1ldweasel <maxhyng@gmail.com>
2019-02-25 00:16:29 +0000

    Fernet encryption working

--- MaxiMoose/cryptography_services.py
+++ MaxiMoose/cryptography_services.py
@@ -6,15 +6,35 @@ Created on Sat Feb 23 22:19:13 2019
 """
 
 from cryptography.fernet import Fernet
+import base64
 
 def main(): 
     generate_key()
+    encrypt_text()
 
 def generate_key():
-    f = open("keyfile.txt", "w")
+    f = open("keyfile.txt", "wb")
     key = Fernet.generate_key()
-    f.write(str(key) + "\r\n")
+    f.write(key)
      
+
+def encrypt_text():
+    f = open("keyfile.txt", "rb")
+    key = f.read()
+    print (key)
+    message = 'plaintext'
+    encoded_message = message.encode()
+    cipher = Fernet(key)
+    cipher_text = cipher.encrypt(encoded_message)
+    print (cipher_text)
+
+def base64_encode():
+    encoded_data = base64.b64encode("Base64 encode this text")
+    print(encoded_data)
+    
+def base64_decode(encoded_text):
+    decoded_data = base64.b64encode(encoded_text)
+    print(decoded_data)
 
 if __name__ == "__main__":
     main()
