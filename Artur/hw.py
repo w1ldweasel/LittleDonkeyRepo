1c096ee8648d5bba832525fd3dab63c5e045ce03
Art <artur.placha@gmail.com>
2019-03-06 12:06:35 +0100

    modified hello world

--- Artur/hw.py
+++ Artur/hw.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from datetime import date
+from datetime import datetime
 
+def user():
+    
+    user.name = input ("What is your name?")
+    while True:
+        try:
+            getdob = input("What is your date of birth? (DD/MM/YYYY)")
+            user.dob = datetime.strptime(getdob, "%d/%m/%Y")
+            break
+        except ValueError:
+            print ("Wrong format, try again...")
+                
+def main():
+    user()
+    try:
+        print (user.name)
+        print (user.dob)
+    except AttributeError:
+        print ("nah")
 
-hw1 = "Hello World!"
-name = input ("What is your name?")
-hw2 = ("Hello " + name)
-print (hw1)
-print (hw2)
-
+main()
