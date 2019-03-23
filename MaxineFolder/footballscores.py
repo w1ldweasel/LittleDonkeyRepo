7ce408c4ddfa0f0296c9f14deeef9be665090ca5
maxxmcf <maxinemcfarlane@yahoo.co.uk>
2019-03-23 13:55:45 +0000

    starting to play a bit with JSON

--- MaxineFolder/footballscores.py
+++ MaxineFolder/footballscores.py
@@ -6,15 +6,22 @@ Created on Wed Mar 20 14:54:53 2019
 @author: maxinemcfarlane
 """
 
-import urllib.request
+import requests
 
-#scrape info from BBC sport
+#get the info from REST API
 def premscores():
-    webUrl = urllib.request.urlopen("https://www.bbc.co.uk/sport/football/premier-league/scores-fixtures/2019-03?filter=results")
-    data = webUrl.read()
-    print(data)
-
-
+    url = 'https://apifootball.com/api/?action=get_countries&APIkey=e7f25b27b4e1d981d76e282bf840c90dee0f243862c5ff26ad5b0775612b0583'
+    r = requests.get(url)
+    print(r.content)
+    
+#trying out string formatting on the API call, may be easier for updating for weekly results
+    sdate = '2016-10-30'
+    edate = '2016-11-01'
+    
+    url2 = ('https://apifootball.com/api/?action=get_events&from=%s&to=%s&league_id=63&APIkey=e7f25b27b4e1d981d76e282bf840c90dee0f243862c5ff26ad5b0775612b0583' %(sdate, edate)) 
+    newr = requests.get(url2)
+    print(newr.content)
+    
 #do win / draw points calculation
 
 
