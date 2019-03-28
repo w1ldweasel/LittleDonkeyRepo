319d0458fd8d55f8f03151bce61036715d33905b
w1ldweasel <maxhyng@gmail.com>
2019-03-28 21:39:15 +0000

    Created application

--- MaxiMoose/Max2WebUI/Max2WebUI/settings.py
+++ MaxiMoose/Max2WebUI/Max2WebUI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sports_data',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
---
+++ MaxiMoose/Max2WebUI/sports_data/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SportsDataConfig(AppConfig):
+    name = 'sports_data'
