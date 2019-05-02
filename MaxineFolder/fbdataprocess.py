2ad86700de5b709f2217311c7f892a83b27aa57c
maxxmcf <maxinemcfarlane@yahoo.co.uk>
2019-05-02 13:58:46 +0100

    api key removal

--- MaxineFolder/fbdataprocess.py
+++ MaxineFolder/fbdataprocess.py
@@ -9,14 +9,41 @@ Created on Wed Apr 24 15:52:48 2019
 from google.cloud import firestore
 import os
 
-#*** NEED TO FIGURE OUT AUTHORISE STEP *** 
 #default_app = firebase_admin.initialize_app()
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/maxinemcfarlane/Downloads/APIkey1.json"
 
-db = firestore.Client()
+#get data from database
+def getdata():
+    db = firestore.Client()
 
-test_ref = db.collection(u'test')
-docs = test_ref.get()
+    test_ref = db.collection(u'test')
+    docs = test_ref.get()
 
-for doc in docs:
-    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+    for doc in docs:
+        print(u'{} => {}'.format(doc.id, doc.to_dict()))
+    
+
+#process data
+#average score, how many wins/draws
+#*** TO BE UPDATED ***
+
+    
+    
+    
+#write data to database
+#THIS WILL WRITE TO A NEW COLLECTION
+#*** TO BE UPDATED ***
+def writedata():
+
+    db = firestore.Client()
+    doc_ref = db.collection(u'test').document(u'thisisafinaltest')
+    doc_ref.set({
+        u'testname': u'Max4',
+        u'testteam': u'Man City',
+        u'points': 80
+    })
+    print("this has worked")
+    
+    
+if __name__ == "__main__":
+    getdata()
--- MaxineFolder/footballdata.py
+++ MaxineFolder/footballdata.py
@@ -10,16 +10,19 @@ import json
 import urllib.request
 from google.cloud import firestore
 import os
+import config
 
 
 #function to set up API call and get data
 #*** LOOK AT CHANGING THIS FROM URLLIB.REQUEST TO REQUESTS (BETTER FOR API)*** 
 def main():
+    
     sdate = '2019-03-15'
     edate = '2019-03-17'
+    key = config.footballkey
     
-    footieurl = ('https://apifootball.com/api/?action=get_events&from=%s&to=%s&league_id=63&APIkey=e7f25b27b4e1d981d76e282bf840c90dee0f243862c5ff26ad5b0775612b0583' %(sdate, edate))
-    
+    footieurl = ('https://apifootball.com/api/?action=get_events&from=%s&to=%s&league_id=63&APIkey=%s' %(sdate, edate, key))
+  
     webUrl = urllib.request.urlopen(footieurl)
     if (webUrl.getcode() == 200):
         data = webUrl.read()
@@ -42,20 +45,27 @@ def printResults(data):
     for i in theJSON:
         print (i["match_hometeam_name"])
         
+        
     writedata()
         
         
 #write data to database
 def writedata():
     
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/maxinemcfarlane/Downloads/APIkey1.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="APIkey1.json"
     
     db = firestore.Client()
-    doc_ref = db.collection(u'test').document(u'thisisafinaltest')
+    doc_ref = db.collection(u'test').document(u'testingmultipleentries')
     doc_ref.set({
-        u'testname': u'Max4',
-        u'testteam': u'Man City',
-        u'points': 80
+        u'testname': u'Klopp',
+        u'testteam': u'Liverpool',
+        u'points': 50
+#        u'testname': u'Pep',
+#        u'testteam': u'Man City',
+#        u'points': 55,
+#        u'testname': u'Silva',
+#        u'testteam': u'Everton',
+#        u'points': 30
     })
     print("this has worked")
         
