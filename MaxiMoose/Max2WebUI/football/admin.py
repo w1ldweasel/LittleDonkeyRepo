e6f405f509d1520a7d665ccf30d2a6ec7c0e2853
w1ldweasel <maxhyng@gmail.com>
2019-06-12 09:37:18 +0100

    Delete

--- MaxiMoose/Max2WebUI/football/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Team
-from .models import Match
-
-admin.site.register(Team)
-admin.site.register(Match)
--- MaxiMoose/Max2WebUI/football/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FootballConfig(AppConfig):
-    name = 'football'
