a48cbc2469e8541fbe40743af616ea869e5fd61b
maxxmcf <maxinemcfarlane@yahoo.co.uk>
2019-08-13 13:56:29 +0100

    basic model E2E

--- MaxineFolder/fbdataprocess.py
+++ MaxineFolder/fbdataprocess.py
@@ -15,7 +15,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="APIkey1.json"
 
 
 def main():
-    #find out the team in question from the user (2468)
+    #find out the team in question from the user (2648)
     #*currently input in this program > FIND OUT HOW TO RECIEVE INPUT FROM MN UI?*
     teamreq = 'm'
     while teamreq != 'Q':
@@ -38,33 +38,38 @@ def getdata(teamid):
     db = firestore.Client()
 
     #to get all documents in a collection, do this
-    getdoc_ref = db.collection('footballtest')
-    docs = getdoc_ref.stream()
+#    getdoc_ref = db.collection('footballtest')
+#    docs = getdoc_ref.stream()
     #getdoc_ref.get > get is depricated for collection, but not document
 
-    for doc in docs:
-        print('{} => {}'.format(doc.id, doc.to_dict()))
+#    for doc in docs:
+#        print('{} => {}'.format(doc.id, doc.to_dict()))
         
     #to get a specific document in a collection, do this
     getdoc2_ref = db.collection('footballtest').document(teamid)
     doc = getdoc2_ref.get()
-    print('Team info: {} {}'.format(doc.id, doc.to_dict()))
-#WHAT TO DO WITH DICT TO BE ABLE TO REF?!
-    testingthis = doc.to_dict()
-    testingthis['goals']
-        
+#    print('Team info: {} {}'.format(doc.id, doc.to_dict()))
+    maxtest = doc.to_dict()
+#    print(maxtest)
+    if maxtest == None:
+        print("That's not a recognised Team ID")
+    else:
+        predresult = maxtest.get("result")
+        teamname = maxtest.get("team name")
     
-#    if result == "win":
- #       print (teamreq, " are predicted to win their next match")
-  #  elif result == "draw":
-#        print (teamreq, " are predicted to drae their next match")
- #   elif result == "lose":
-  #      print (teamreq, " are predicted to lose their next match") 
-#    else:
- #       print ("We have no data on ", teamreq)
+        if predresult == "win":
+            print('\n\n')
+            print(teamname, " are predicted to win their next match") 
+        elif predresult == "draw":
+            print('\n\n')
+            print(teamname, " are predicted to draw their next match")
+        elif predresult == "lose":
+            print('\n\n')
+            print(teamname, " are predicted to lose their next match") 
+        else:
+            print('\n\n')
+            print(teamname, "We have no data on ")
  
-# JULY UPDATES
-# IF WON LAST GAME, WILL WIN, ETC.
 # ADD MORE VARIABLES AND ADD WEIGHTING
       
     
